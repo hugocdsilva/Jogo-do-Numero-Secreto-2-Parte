9389ebb0f7cd965dbc3281029354ce0d63c88564
Hugo Silva <hugocorreia1997@gmail.com>
2024-02-03 21:53:27 -0300

    Atualização Jogo do Número secreto

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+/*let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha o número entre 1 e 10';
+*/
+let numeroAleatorio = gerarNumeroAleatorio();
+
+function exibirNomeNaTela (tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirNomeNaTela('h1','Jogo do número secreto');
+exibirNomeNaTela('p', 'Escolha o número entre 1 e 10')
 
 function verificarChute() {
-    console.log('O botão foi clicado!')
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroAleatorio);
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 +1);
 }
